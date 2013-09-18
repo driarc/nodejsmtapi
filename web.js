ad21185c99887181aa28a9a40baf7bf94dd5404f
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-09-19 00:59:02 +0530

    fixed bug fir processHTML special comment processing, added Wid to output JSON

--- web.js
+++ web.js
@@ -29,52 +29,6 @@ app.configure('development', function(){
 });
 
 
- 
-app.param('collectionName', function(req, res, next, collectionName){
-  req.collection = db.collection(collectionName)
-  return next()
-})
-
-app.get('/', function(req, res) {
-  res.send('please select a collection, e.g., /collections/messages')
-})
- 
-app.get('/collections/:collectionName', function(req, res) {
-  req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
-    if (e) return next(e)
-    res.send(results)
-  })
-})
- 
-app.post('/collections/:collectionName', function(req, res) {
-  req.collection.insert(req.body, {}, function(e, results){
-    if (e) return next(e)
-    res.send(results)
-  })
-})
- 
- 
-app.get('/collections/:collectionName/:id', function(req, res) {
-  req.collection.findOne({_id: req.collection.id(req.params.id)}, function(e, result){
-    if (e) return next(e)
-    res.send(result)
-  })
-})
-app.put('/collections/:collectionName/:id', function(req, res) {
-  req.collection.update({_id: req.collection.id(req.params.id)}, {$set:req.body}, {safe:true, multi:false}, function(e, result){
-    if (e) return next(e)
-    res.send((result===1)?{msg:'success'}:{msg:'error'})
-  })
-})
-app.del('/collections/:collectionName/:id', function(req, res) {
-  req.collection.remove({_id: req.collection.id(req.params.id)}, function(e, result){
-    if (e) return next(e)
-    res.send((result===1)?{msg:'success'}:{msg:'error'})
-  })
-})
- 
-
-
 //// *********************** Logic for the Application follows   *********************** 
 
 /// logic for executeThis
