5ab615bc2529c886ed6cbacdfdb0916b7ec4f635
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-09-25 00:14:09 +0530

    test updateWid changes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 exports.TABLE_NAME = 'colsam';
 
-// exports.MONGODB_URL = 'mongodb://trugate:tempalte-77@ds045627.mongolab.com:45627/wikiwallettesting?auto_reconnect';
+exports.MONGODB_URL = 'mongodb://trugate:tempalte-77@ds045627.mongolab.com:45627/wikiwallettesting?auto_reconnect';
 
-exports.MONGODB_URL = 'mongodb://localhost:27017/nodejsmtapi?auto_reconnect';
+// exports.MONGODB_URL = 'mongodb://localhost:27017/nodejsmtapi?auto_reconnect';
 
 exports.MONGODB_OPTIONS = {'safe':true,'server':true,'auto_reconnect': true, 'pool': 5} ;
 exports.LOOKUP_DIR = '../dripoint/';
