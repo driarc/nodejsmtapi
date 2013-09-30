f613508365e6e578454df01f1f506e6a9c3f0eec
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-09-30 11:50:59 +0530

    added preexeute logic, made changes to GEtWid param name FromWid

--- dao/mongo.js
+++ dao/mongo.js
@@ -119,7 +119,6 @@ exports.addOrUpdate = function(entityToAdd,schemaToLookup, callback){
     if(!widVal){
     	widVal = (entityToAdd['Wid']);
     }
-    
     console.log('addOrUpdate :::: widVal is >>> '+widVal);
 	getFromMongo({"wid":widVal},schemaToLookup,function(returnedObject){
         console.log(' >>>> addOrUpdate ::: Default case >>> DB returns >>>  '+ JSON.stringify(returnedObject));
