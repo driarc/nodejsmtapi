5e45e8228cd7d7dc521da02d2d5e191399d8b36b
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-09-30 15:10:40 +0530

    added preexeute logic, made changes to GEtWid param name FromWid

--- routes/server.js
+++ routes/server.js
@@ -15,7 +15,7 @@ var TABLE_NAME = config.TABLE_NAME;
 console.log(TABLE_NAME +' is the name of the table !!! ');
 
 /// logic for executeThis
-exports.executethis = function(req, res) {
+exports.executethisOld = function(req, res) {
     var inboundParameters = new HashMap();
     var leftOverParameters = new HashMap();
     var reservedParameters = new HashMap();
@@ -558,7 +558,7 @@ function callScrapeLogic(res, callback){
     });
 }
 
-function cleanupParameters(inboundParameters,paramsToClean){
+function cleanupParametersOld(inboundParameters,paramsToClean){
     var outBoundParameters = inboundParameters;
     
     for(var i=0;i<paramsToClean.length;i++){
@@ -569,7 +569,7 @@ function cleanupParameters(inboundParameters,paramsToClean){
     return outBoundParameters;
 }
 
-function mergeParameters(c1,c2){
+function mergeParametersOld(c1,c2){
     var mergedMap = c1;
 
     for(key in c2._data){
@@ -578,3 +578,220 @@ function mergeParameters(c1,c2){
 
     return c1;
 } 
+
+
+/// logic for executeThis
+exports.executethis = function(req, res) {
+
+    var item = req.body[0];
+   
+   if (item && item.addthis && item.executethis) {
+        //  handle AddThis as a command 
+        console.log(' AddThis operation. ' + JSON.stringify(req.body));
+
+        // START PROCESSING AS PER 'addThis' param of the JSON in the request body received
+        filecheck.handleAddThis(req.body,function(returnedObject){
+            console.log('coming back after addthis'+JSON.stringify(returnedObject));
+
+            // save returned data to DB
+            callUpdateWid(returnedObject.addThisJson[0], function(o){
+                res.send({"message":"Addd Wid successfully "});
+                res.end();
+            });
+        });
+        console.log('After addThis logic has been processed');
+        console.log('------------------------------------------------');
+
+    }else if (item && item.executethis) {
+        console.log('------------------------------------------------');
+        console.log(' <<<<<<<<<<<<<<< PRe , ExecuteThis AND  Post operation. >>>>>>>>>>>>>>>>>>>> ');
+
+
+            var funcT = reservedParameters.get("executethis");
+            switch (funcT.toLowerCase()) {
+
+                case 'extractthis':
+                    //  handle extractThis case
+                    // call the file check codeZ
+                    callScrapeLogic(res, function(nodeObjects){
+                        if(nodeObjects){
+                            handleProcessHtmlPersistence(nodeObjects, function(){
+                                handleAddThisPersistence(nodeObjects, function(){
+                                    callback(nodeObjects);
+                                });
+                            });
+                        }
+                    });
+                
+                    break;        
+
+                case 'getfrommongo':
+                    // handle get from mongo DB logic
+
+                    // call get from mongo DB 
+                    var rec = {"wid":item.wid};
+                    console.log("Fetching one record "+JSON.stringify(rec));
+                    dao.getFromMongo(rec,config.TABLE_NAME,function(obj){
+                        console.log("Fetched from Mongo DB  - "+ JSON.stringify(obj));
+                        res.send(obj);
+                        res.end();
+                    });
+                    break;    
+
+                case 'getwid':
+                    // handle getwid :: 
+                    if(item.fromwid){
+                        // call get from mongo DB 
+                        var widVal = item.fromwid;
+                        console.log("getwid ::: Fetching one record , with fromWid "+widVal);
+                        getFromMongo({"wid":widVal},config.TABLE_NAME,function(obj){
+
+                            if(obj){
+                                // get JSOn from DB
+                                console.log("getwid ::: Fetched from Mongo DB  - "+ JSON.stringify(obj));
+                                res.send(obj);
+                                res.end();
+                            }else{
+                                // get JSOn from DB
+                                console.log("getwid ::: No Wid matching value, return empty JSON - ");  
+                               res.send({});
+                               res.end(); 
+                            }
+                        });
+                    }else{
+                       console.log("getwid ::: No Wid specified, return empty JSON - ");  
+                       res.send({});
+                       res.end();  
+                    }
+            
+                    break;     
+
+                case 'updatewid':
+                    
+                    // handle updatewid
+                    if(item.wid){
+                        // get JSOn to be saved
+                        var entityToAdd = cleanParameters(item,["executethis","wid","preexecute","postexecute","fromwid"]);
+                        
+                        // if fromProperty exists, then copy that as a new property
+                        if(entityToAdd.fromproperty){
+                            delete  entityToAdd.fromproperty;
+                        }
+                        
+                        // if toProperty exists, then copy that as a new property
+                        if(entityToAdd.toproperty){
+                            delete  entityToAdd.toproperty;
+                        }
+                        
+                        // if status exists and equals 5, then delete the wid data
+                        if(item.status){
+                            if(item.status === 5){
+                                dao.removeFromMongo({"wid":entityToAdd.wid},config.TABLE_NAME,function(o){
+                                    console.log("updatewid :: After deleting node from Mongo - "+ JSON.stringify(o));  
+                                    res.send(o);
+                                    res.end();   
+                                });
+                            }
+                        }else{
+                            console.log("updatewid :: Add/update record "+JSON.stringify(entityToAdd));
+
+                            // call persistence method
+                            dao.addOrUpdate(entityToAdd,config.TABLE_NAME,function(o){
+                               console.log("updatewid :: After adding/updating node to Mongo - "+ JSON.stringify(o));  
+                               res.send(o);
+                               res.end();   
+                            });
+                        }
+                        
+                    }else{
+                       console.log("updatewid ::: No Wid specified, Do Nothing - ");  
+                       res.send({});
+                       res.end();  
+                    }
+                    break;            
+
+                        
+                case 'addtomongo':
+                    // handle ADD TO MONGO logic
+                    var rec = {};
+                    if(typeof item.wid === 'object'){
+                        rec = item.wid;
+                    }else{
+                        rec = cleanParameters(item,["executethis","wid","preexecute","postexecute","fromwid"]);
+                    }
+                    console.log("Add to mongo record -- "+JSON.stringify(rec));
+                    dao.addOrUpdate(rec,TABLE_NAME,function(o){
+                        console.log("After adding to Mongo - "+ JSON.stringify(o));
+                        callback(o);
+                    });
+                    break;
+
+                default:
+                    // for example if executeThis = abc and abc is not found as a case the system is supposed to go get from mongo
+                    // (abc)…and use the results as parameters to call again - recurse into executeThis
+
+                    // if I send executeThis {executetThis=Saurabh}
+                    // in case of no case match for 'Saurabh',
+                    // parms= getfrommongo (wid=Saurabh)
+                    // call executeThis(parms)
+
+                    // if wid=Saurabh = {executeThis:extractThis} then extractThis will run
+                    var executeThisVal = funcT.toLowerCase();
+                    console.log('default parameter value for executetThis is >>> '+executeThisVal);
+
+                    // call get from mongo DB 
+                    var queryObj = {'wid':executeThisVal};
+                    dao.getFromMongo(queryObj,TABLE_NAME, function(returnedObject){
+                        console.log('>>>>>>> Default case >>> DB returns >>>  '+ JSON.stringify(returnedObject));
+
+                        // check if object is found
+                        if(returnedObject){
+                            // Make another request, matching data found in MongoDB
+                            superagent.put(config.SERVICE_URL+'executethis')
+                                .send(returnedObject)
+                                  .end(function(e, res){
+                                    console.log('>>>>>>>>> Sent another request :: ExtractThis :: PUT request ');
+                                    
+                                        
+                            });
+                          callback(returnedObject);
+                        }else{
+                            // nothing to do, no matching data found in MongoDB
+                            console.log('>>>>>>>>> nothing to do, no matching data found in MongoDB');
+                            res.send({});
+                            res.end();
+                        }
+                    });
+
+                    break;
+            }
+    }
+    else {
+        res.send({
+            "error": "Requires a valid parameter : executeThis"
+        });
+    }
+};
+
+
+function cleanupParameters(inboundParameters,paramsToClean){
+    var outBoundParameters = inboundParameters;
+    
+    for(var i=0;i<paramsToClean.length;i++){
+        if(outBoundParameters[paramsToClean[i]]){
+            delete outBoundParameters[paramsToClean[i]];
+        }
+    } 
+    return outBoundParameters;
+}
+
+function mergeParameters(c1,c2){
+    var mergedMap = c1;
+
+    for(key in c2 && (!c1[key])){
+        c1[key] = c2[key];
+    };
+
+    return c1;
+} 
+
