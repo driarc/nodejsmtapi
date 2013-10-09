0421e4848ecc87df7bc29994aa429f783e66739f
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-10-09 20:29:55 +0530

    fixed regex issue in extract this

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ exports.MONGODB_URL = 'mongodb://trugate:tempalte-77@ds045627.mongolab.com:45627
 // exports.MONGODB_URL = 'mongodb://localhost:27017/nodejsmtapi'
 
 exports.MONGODB_OPTIONS = {'safe':true,'server':true,'auto_reconnect': true, 'pool': 5} ;
-// exports.LOOKUP_DIR = '../dripoint/';
-exports.LOOKUP_DIR = '../html/';// for testing
+exports.LOOKUP_DIR = '../dripoint/';
+// exports.LOOKUP_DIR = '../html/';// for testing
 exports.SERVICE_URL = 'http://localhost:3000/';
