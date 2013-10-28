6c5df7168c8740e35ae6eb19a60b944ed738dbcc
JaySturm <jaysturm@gmail.com>
2013-10-28 12:38:38 -0600

    Adding .wml to .html processes

---
+++ routes/driTemplate.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var cheerio = require('cheerio')
+  , fs = require('graceful-fs')
+  , walk = require('walk')
+  , moment = require('moment')
+  , config = require('../config.js')
+  , HashMap = require('hashmap').HashMap
+  , lookupDir = config.LOOKUP_DIR
+  , $ = undefined;
+
+function buildTemplate(parameters) {
+	var masterWml = parameters.wmlfilename;
+	console.log('**driTemplate.buildTemplate** Getting file contents of - ' + masterWml);
+	var masterContents = findAndReadFile(lookupDir, masterWml);
+
+	// find [[<wmlFileName>]] tags and replace with contents of <wmlFileName>.wml
+	var regex = new RegExp('[[', 'g')
+	  , nextWml = ''
+	  , masterPath = ''
+	  , result;
+
+	console.log('**driTemplate.buildTemplate** Starting to handle [[<wml>]] tags');
+	while ((result = regex.exec(masterContents))) {
+		nextWml = result.substr(2, result.indexof(']]'));  // remove [[ and ]]
+		console.log('**driTemplate.buildTemplate** Replacing ' + result + ' tag.');
+		masterContents.replace(result, findAndReadFile(lookupDir, nextWml + '.wml'));
+	}
+	console.log('**driTemplate.buildTemplate** Finished handling [[<wml>]] tags');
+
+	// (?)TODO(?) find {{<binding>}} tags and create a widForView js variable which will be a csv of all the bound wids (?)
+
+	// in here cheerio can take the entire codefile and modify it through it's jQuery interface
+	$ = cheerio.load(masterContents, {
+	    ignoreWhitespace: true,
+	    xmlMode: false
+	});
+
+	// save codeFile aggregation under original <masterWml>.html in the same directory as <masterWml>.wml
+	masterPath = findAndReadFile(lookupDir, masterWml, true);
+	fs.writeFile(masterPath + '/' + masterWml + '.html', masterContents, function(err) {
+		if (err) { throw err; }
+		console.log('**driTemplate.buildTemplate** Created ' + masterPath + '/' + masterWml + '.html file.');
+	});
+}
+
+function buildAllTemplates() {  // don't know if we want to do this as there will be master wml files and partial wml files.
+	var walker  = walk.walk(lookupDir, { followLinks: false });
+
+	walker.on('file', function(path, fileStats, next) {
+		if (fileStats.name.endsWith('.wml')) { buildTemplate({wmlfilename:fileStats.name}); }
+	    next();
+	});
+}
+
+function findAndReadFile(startDir, fileName, returnPath) {
+	returnPath = returnPath || false;
+	var walker = walk.walk(startDir, {followLinks: false });	
+	walker.on('file', function(path, fileStats, next) {
+		if (fileStats.name.substr(0, fileStats.name.indexof('.'))) {
+			if (returnPath) { return path; }
+			else { return fs.readFileSync(path + '/' + fileStats.name); }
+		}
+	});
+}
--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var express = require('express')
   , config = require('./config.js')
   , common = require('./routes/common')
   , server = require('./routes/server')
+  , driTemplate = require('./routes/driTemplate')
 
 
  
@@ -51,6 +52,7 @@ app.configure('development', function(){
 app.get('/', common.index);
 app.get('/test', common.test);
 app.put('/executethis', server.executethis);
+app.put('/buildtemplate', driTemplate.buildTemplate);
 
 app.get('/updateWidTest', common.updateWidTest);
 
