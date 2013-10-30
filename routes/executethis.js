9886d404e7d3c7ed2eed3321cf13d3aefb1cf500
JaySturm <jaysturm@gmail.com>
2013-10-30 16:13:06 -0600

    changing debug code for executethis:test test

--- routes/executethis.js
+++ routes/executethis.js
@@ -35,7 +35,7 @@ if(!global){
         var params = util.toLowerKeys(inboundparms);
 
         console.log('targetfunction is a function => ' + typeof targetfunction === 'function');
-        console.log('is equal to execute => ' targetfunction === execute);
+        console.log('is equal to execute => ' + targetfunction === execute);
         if (typeof targetfunction === 'function'){
             if (targetfunction === execute) {
                 targetfunction(params, function(data) {
