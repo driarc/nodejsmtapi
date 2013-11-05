845ec7691bbfdc44e2681a9ee16b78a0807385ff
JaySturm <jaysturm@gmail.com>
2013-11-04 17:07:55 -0700

    adding wml processing code to driTemplate

--- routes/driTemplate.js
+++ routes/driTemplate.js
@@ -1,16 +1,16 @@
 'use strict';
 var cheerio = require('cheerio')
-  , $ = undefined
-  , fs = require('graceful-fs')
-  , find = require('findit')
-  , config = require('../config.js')
-  , lookupDir = config.LOOKUP_DIR
-  , masterContents = {code:''}
-  , htmlPath
-  , response
-  , responseData = {};
+    , $ = undefined
+    , execute = require('executethis.js')
+    , fs = require('graceful-fs')
+    , find = require('findit')
+    , config = require('../config.js')
+    , lookupDir = config.LOOKUP_DIR
+    , masterContents = {code:''}
+    , responseData = {}
+    , htmlPath, response, processWml, buildTemplate;
 
-exports.buildTemplate = function(req, res) {
+exports.buildTemplate = buildTemplate = function(req, res) {
 	console.log('*******START******** driTemplate.buildTemplate ********START*********');
 
 	response = res;
@@ -22,19 +22,65 @@ exports.buildTemplate = function(req, res) {
 
 		for (var i = 0; i < tags.length; i++) {
 			var nextWml = tags[i].replace('[[', '').replace(']]', '');
-			findAndReadFile(lookupDir, nextWml, tags[i], replaceWmlTag);
+			findAndReadFile(lookupDir, nextWml, 'wml', tags[i], replaceWmlTag);
 		}
 
 		htmlPath = masterPath.replace('.wml', '.html');
 		responseData.filepath = htmlPath;
 	});
-}
+};
+
+exports.processWml = processWml = function(req, res) {
+    console.log('*******START******** driTemplate.processWml ********START*********');
+
+    response = res;
+    var parameters = req.body;
+
+    getWmlTags(parameters.wmlfilename, function(tags, masterPath) {
+        console.log(' retrieved [[<wml>]] tags from => ' + masterPath);
+        console.log(' [[<wml>]] tags found => ' + JSON.stringify(tags));
+
+        for (var i = 0; i < tags.length; i++) {
+            var tagValue = tags[i].replace('[[', '').replace(']]', '')
+                , tagObject;
+
+            try { tagObject = JSON.parse(tagValue); }
+            catch(e) { console.log(tags[i] + ' is not a JSON object.'); }
+
+            if (tagObject) {
+                var action = tagObject.executethis || '';
+
+                if (action === 'getfile') {
+//                    if (!tagObject.fromfile || !tagObject.fromextension) {
+//                        responseData.error = "fromfile or fromextension or both are missing on this getfile tag.";
+//                        response.send(responseData);
+//                        response.end();
+//                    }
+
+                    var getFilename = tagObject.fromfile + '.' + tagObject.fromextension;
+
+                    // replace with specific area else entire contents of file
+                    if (tagObject.fromarea) { findAndReadFile(getFilename, tagObject.fromarea, tags[i], replaceTagWithArea); }
+                    else { findAndReadFile(getFilename, '', tags[i], replaceWmlTag); }
+                } else {
+                    // process the executeThis call
+                    var params = {executethis:action};
+                    // TODO: figure out what to do with the executeThis results
+                    responseData.executeresults = execute.executethis(params);
+                }
+            } else { findAndReadFile(tagValue + '.wml', '', tags[i], replaceWmlTag); }
+        }
+
+        htmlPath = masterPath.replace('.wml', '.html');
+        responseData.filepath = htmlPath;
+    });
+};
 
 // get [[<wml>]] tags from contents of master wml file
 function getWmlTags(filename, callback) {
-	findAndReadFile(lookupDir, filename, '', function(file) {
+	findAndReadFile(filename + '.wml', '', '', function(file) {
 		masterContents.code = file.contents;
-		var regex = new RegExp('\\[\\[.*]]', 'g')
+		var regex = new RegExp('\\[\\[.*\\]\\]', 'g')
 		  , wmlTags = []
 		  , result;
 
@@ -63,7 +109,7 @@ function changedContents() {
 				console.log(' Successfully created file => ' + htmlPath);
 			}
 
-			console.log('*********END******* driTemplate.buildTemplate *********END***********');
+			console.log('*************END********** driTemplate ************END***************');
 			response.send(responseData);
 			response.end();
 		});
@@ -71,13 +117,28 @@ function changedContents() {
 }
 
 // find a file recursively and return it's path and contents
-function findAndReadFile(startDir, fileName, tag, callback) {
-	var finder = find(startDir);
-	finder.on('file', function(file, stat) {
-		if (file.endsWith(fileName + '.wml')) {
+function findAndReadFile(fileName, area, tag, callback) {
+	var finder = find(lookupDir);
+	finder.on('file', function(file, stats) {
+		if (file.endsWith(fileName)) {
 			var results = { path:file, contents:fs.readFileSync(file).toString() };
 			if (tag && tag !== '') results.tag = tag;
+            if (area && area !== '') results.area = area;
 			callback(results);
 		}
 	});
+}
+
+// extract area from found file
+function replaceTagWithArea(file) {
+    var pattern = '\<!--{"areadefinition": "begin", "areaname": "' + file.area + '"}--\>(.*)'
+                  + '\<!--{"areadefinition": "end", "areaname": "' + file.area + '"}--\>'
+        , regex = new RexExp(pattern, 'gi')
+        , result;
+
+    while (result = regex.exec(masterContents.code)) {
+        masterContents.code = masterContents.code.replace(file.tag, result);
+    }
+
+    changedContents();
 }
