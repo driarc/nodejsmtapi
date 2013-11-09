892605148ba9f31e5e431334f19f95714662d02a
JaySturm <jaysturm@gmail.com>
2013-11-08 17:20:12 -0700

    debugging changes

--- dao/addget.js
+++ dao/addget.js
@@ -17,7 +17,7 @@ function printToDiv(text, obj, debugone){
     }
 }
 
-exports.getwid = getwid = function (inputWidgetObject, callback) {
+exports.getwid = global.getwid = getwid = function (inputWidgetObject, callback) {
     proxyprinttodiv('Function getwid in : inputWidgetObject',  inputWidgetObject);
     //resultObj = executethis(inputWidgetObject);
     resultObj=executethis(inputWidgetObject,getfrommongo);
@@ -27,7 +27,7 @@ exports.getwid = getwid = function (inputWidgetObject, callback) {
     else { return resultObj; }
 };
 
-exports.updatewid = updatewid = function (inputWidgetObject, callback) {
+exports.updatewid = global.updatewid = updatewid = function (inputWidgetObject, callback) {
     // todelete added 11/2 -- optional parm ) otherwise default to executethis -- used for addthis
     proxyprinttodiv('Function updatewid in : inputWidgetObject',  inputWidgetObject,1);
     // if (inputWidgetObject["addthisparameters"]) { // added 11/2
