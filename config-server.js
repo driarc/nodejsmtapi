575893e96236c55011c75cd1c72f608e8f358b4e
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-11-19 01:00:06 +0530

    changes for server side

--- config-server.js
+++ config-server.js
@@ -8,47 +8,59 @@ var config123 = function() {
 
     configuration.environment='server';
 
-    configuration.preexecute = [];
-    configuration.preexecute[0] = {};
-    configuration.preexecute[0].executeorder = 0;
-    configuration.preexecute[0].tryorder = 0;
-    configuration.preexecute[0].dothis = 'executeFn';
-    configuration.preexecute[1] = {};
-    configuration.preexecute[1].executeorder = 0;
-    configuration.preexecute[1].tryorder = 0;
-    configuration.preexecute[1].dothis = 'executeParam';
-    configuration.preexecute[2] = {};
-    configuration.preexecute[2].executeorder = 0;
-    configuration.preexecute[2].tryorder = 0;
-    configuration.preexecute[2].dothis = 'executeDefault';
+configuration.preExecute = [];
+configuration.preExecute[0] = {};
+configuration.preExecute[0].executeorder = 0;
+configuration.preExecute[0].tryorder = 0;
+configuration.preExecute[0].dothis = 'executeFn';
+configuration.preExecute[1] = {};
+configuration.preExecute[1].executeorder = 0;
+configuration.preExecute[1].tryorder = 0;
+configuration.preExecute[1].dothis = 'executeParam';
+configuration.preExecute[2] = {};
+configuration.preExecute[2].executeorder = 0;
+configuration.preExecute[2].tryorder = 0;
+configuration.preExecute[2].dothis = 'executeGetWid';
 
-    configuration.midexecute = [];
-    configuration.midexecute[0] = {};
-    configuration.midexecute[0].executeorder = 1;
-    configuration.midexecute[0].tryorder = 2;
-    configuration.midexecute[0].dothis = 'executeFn';
-    configuration.midexecute[1] = {};
-    configuration.midexecute[1].executeorder = 1;
-    configuration.midexecute[1].tryorder = 1;
-    configuration.midexecute[1].dothis = 'executeParam';
-    configuration.midexecute[2] = {};
-    configuration.midexecute[2].executeorder = 1;
-    configuration.midexecute[2].tryorder = 1;
-    configuration.midexecute[2].dothis = 'executeDefault';
+configuration.midExecute = [];
+configuration.midExecute[0] = {};
+configuration.midExecute[0].executeorder = 1;
+configuration.midExecute[0].tryorder = 2;
+configuration.midExecute[0].dothis = 'executeFn';
+configuration.midExecute[1] = {};
+configuration.midExecute[1].executeorder = 1;
+configuration.midExecute[1].tryorder = 1;
+configuration.midExecute[1].dothis = 'executeParam';
+configuration.midExecute[2] = {};
+configuration.midExecute[2].executeorder = 1;
+configuration.midExecute[2].tryorder = 1;
+configuration.midExecute[2].dothis = 'executeGetWid';
 
-    configuration.postexecute = [];
-    configuration.postexecute[0] = {};
-    configuration.postexecute[0].executeorder = 0;
-    configuration.postexecute[0].tryorder = 0;
-    configuration.postexecute[0].dothis = 'executeFn';
-    configuration.postexecute[1] = {};
-    configuration.postexecute[1].executeorder = 0;
-    configuration.postexecute[1].tryorder = 0;
-    configuration.postexecute[1].dothis = 'executeParam';
-    configuration.postexecute[2] = {};
-    configuration.postexecute[2].executeorder = 0;
-    configuration.postexecute[2].tryorder = 0;
-    configuration.postexecute[2].dothis = 'executeDefault';
+configuration.postExecute = [];
+configuration.postExecute[0] = {};
+configuration.postExecute[0].executeorder = 0;
+configuration.postExecute[0].tryorder = 0;
+configuration.postExecute[0].dothis = 'executeFn';
+configuration.postExecute[1] = {};
+configuration.postExecute[1].executeorder = 0;
+configuration.postExecute[1].tryorder = 0;
+configuration.postExecute[1].dothis = 'executeParam';
+configuration.postExecute[2] = {};
+configuration.postExecute[2].executeorder = 0;
+configuration.postExecute[2].tryorder = 0;
+configuration.postExecute[2].dothis = 'executeGetWid';
+configuration.getwid = [];
+configuration.getwid[0] = {};
+configuration.getwid[0].order = 0;
+configuration.getwid[0].dothis = 'getwid';  
+configuration.updatewid = [];
+configuration.updatewid[0] = {};
+configuration.updatewid[0].order = 0;
+configuration.updatewid[0].dothis = 'updatewid';
+configuration.querywid = [];
+configuration.querywid[0] = {};
+configuration.querywid[0].order = 0;
+configuration.querywid[0].dothis = 'querywid';
 
     return {
         "configuration": configuration
@@ -60,9 +72,9 @@ exports.this  = config =  config123();
 
 // exports.configuration=configuration;
 // Primary execute function called after doThis
-exports.test21 = global.test21 = function test21(params, callback){
+exports.test21 = global.test21 =  function test21(params, callback){
     console.log(" test  21" );
-    callback({"test":"test21 on local called"});
+    callback({"test":"test21 on server called"});
 }
 
 
