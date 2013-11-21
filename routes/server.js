2d836f2a25111b84242f473b0348349f8bd67e92
JaySturm <jaysturm@gmail.com>
2013-11-21 15:10:14 -0700

    keeping changes permanently as setting the require statements equal to variables fixed scope issues that we were running into

--- routes/server.js
+++ routes/server.js
@@ -1,24 +1,9 @@
-// Server specific Routes here
-
-//require('../../dripoint/js/et-utils.js');
-//require('../../dripoint/js/et-get.js');
-//require('../../dripoint/js/et-add.js');
-//require('../../dripoint/js/et-query.js');
-//var executethis = require('../../dripoint/js/executethis.js');
-
-
-// require('../../dripoint/saurshaz/js/et-utils.js');
-// require('../../dripoint/saurshaz/js/et-get.js');
-// require('../../dripoint/saurshaz/js/et-add.js');
-// require('../../dripoint/saurshaz/js/et-query.js');
-// var executethis = require('../../dripoint/saurshaz/js/executethis.js');
-
 var mongoskin = require('mongoskin')
-    , utils = require('../../dripoint/js/et-utils.js')
-    , etget = require('../../dripoint/js/et-get.js')
-    , etadd = require('../../dripoint/js/et-add.js')
-    , query = require('../../dripoint/js/et-query.js')
-    , executethis = require('../../dripoint/js/executethis.js')
+  , utils = require('../../dripoint/js/et-utils.js')
+  , etget = require('../../dripoint/js/et-get.js')
+  , etadd = require('../../dripoint/js/et-add.js')
+  , query = require('../../dripoint/js/et-query.js')
+  , executethis = require('../../dripoint/js/executethis.js')
   , config = require('../config-server.js')
   , settings = require('../settings.js')
   , db = mongoskin.db(settings.MONGODB_URL, settings.MONGODB_OPTIONS)
