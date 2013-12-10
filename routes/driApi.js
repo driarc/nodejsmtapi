1e24b3885d4325a4a4f08902d526260989b4e3ae
JaySturm <jaysturm@gmail.com>
2013-12-09 17:36:10 -0700

    modifying debug code for driApi

--- routes/driApi.js
+++ routes/driApi.js
@@ -1,8 +1,7 @@
 'use strict';
-var https = require('https')
-    , querystring = require('querystring')
-//    , host = 'http://localhost:3000'
-    , host = 'http://dripoint.com'
+var needle = require('needle')
+//    , querystring = require('querystring')
+    , host = 'http://localhost:3000'
     , apiKey = '2FFA4085C7994016913F8589B765D4E5'
     , driGetData;
 
@@ -10,53 +9,60 @@ exports.driGetData = driGetData = function driGetData(req, resp) {
     var params = req.body;
 
     // get results from dri Api
-    getData(params, function(results) {
-        resp.send(results);
+    getData(params, function(err, results) {
+        var returnData = err || results;
+        resp.send(returnData);
         resp.end();
     });
 };
 
-function getData(params, successFn) {
+function getData(params, callback) {
     var actionQueryString = params.actionQueryString || ''
         , putUrl = actionQueryString.indexOf('?') !== -1
             ? '/getdataDIRECT/' + actionQueryString + '&apiKey=' + apiKey  // no url params found
             : '/getdataDIRECT/' + actionQueryString + '?apiKey=' + apiKey  // url params already present
-        , options = {
-            hostname: host,
-            path: putUrl,
-            method: 'PUT',
-            headers: {'content-type':'Application/Json'}
-        };
+        , options = { json: true };
 
     console.log(putUrl);
 
-    var paramString = JSON.stringify(params.parameterDTOs);
-
-    // set up request
-    var req = https.request(options, function(res) {
-        var resultString = '';
-        res.setEncoding('utf-8');
-
-        // build result data as it comes in
-        res.on('data', function(chunk) {
-            console.log('chunk found! => ' + chunk);
-            resultString += chunk;
-        });
-
-        // when all data is returned then send it off in the success callback
-        res.on('end', function() {
-            console.log('results from dri getdata call => ' + resultString);
-            var resultObj = JSON.parse(resultString);
-            successFn(resultObj);
-        });
+    needle.put(putUrl, params.parameterDTOs, options, function(err, resp, body) {
+        if (err) {
+            console.log('error occurred during getdata call => ' + err);
+            callback(err, body)
+        }
+        else {
+            console.log('results from dri getdata call => ' + body);
+            callback(null, body);
+        }
     });
 
-    req.on('error', function(err) {
-        console.log('The following error occurred during getdata request => ' + err.message);
-        req.end();
-    });
-
-    // submit request, passing in paramString, then end request
-    req.write(paramString);
-    req.end();
+//    var paramString = JSON.stringify(params.parameterDTOs);
+//
+//    // set up request
+//    var req = http.request(options, function(res) {
+//        var resultString = '';
+//        res.setEncoding('utf-8');
+//
+//        // build result data as it comes in
+//        res.on('data', function(chunk) {
+//            console.log('chunk found! => ' + chunk);
+//            resultString += chunk;
+//        });
+//
+//        // when all data is returned then send it off in the success callback
+//        res.on('end', function() {
+//            console.log('results from dri getdata call => ' + resultString);
+//            var resultObj = JSON.parse(resultString);
+//            successFn(resultObj);
+//        });
+//    });
+//
+//    req.on('error', function(err) {
+//        console.log('The following error occurred during getdata request => ' + err.message);
+//        req.end();
+//    });
+//
+//    // submit request, passing in paramString, then end request
+//    req.write(paramString);
+//    req.end();
 }
